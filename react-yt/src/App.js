c162757223f71fea9249950fa7b08f890345c23d
Achinit99 <achinit720@gmail.com>
2023-01-21 11:48:00 +0530

    new veriable ( External Variable)
    new veriable ( External Variable ) ekak hadagena ekta component eka return karala adala veriable ekena acsess karanawa

--- react-yt/src/App.js
+++ react-yt/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Main from "./Componets/Main"
 import myData from "./Data/myData";
 
+const MainBlock = myData.map(({ name, city, position, id }) => {
+  return <Main key={id} name={name} city={city} position={position} />;
+})
+
 function App() {
   return (
     <>
       <div>
         <p>My App</p>
-        {myData.map(({ name, city, position, id }) => {
-          return <Main key={id} name={name} city={city} position={position} />;
-        })}
+        {MainBlock}
       </div>
     </>
   );
