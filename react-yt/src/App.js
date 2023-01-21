d04fc95443b39315da5f11e14c2c5f8196758c37
Achinit99 <achinit720@gmail.com>
2023-01-21 12:32:19 +0530

    Fragment
    meka Emty Fragment eka wagema use krnna puluvn <> </>

--- react-yt/src/App.js
+++ react-yt/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Main from "./Componets/Main"
 import myData from "./Data/myData";
+import { Fragment } from 'react';
 
 const NewBlock = () => {
   return (
-    <>
+    <Fragment>
       {
         myData.map(({ name, city, position, id }) => {
           return <Main key={id} name={name} city={city} position={position} />;
         })
       }
-    </>
+    </Fragment>
   )
 }
 
