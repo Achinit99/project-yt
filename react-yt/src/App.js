8f6b745ec39f0a3fceb8e9879b5dfbc0bb9a45cc
Achinit99 <achinit720@gmail.com>
2023-01-25 08:24:08 +0530

    Fragment

--- react-yt/src/App.js
+++ react-yt/src/App.js
@@ -3,30 +3,32 @@ import Main from "./Componets/Main"
 import myData from "./Data/myData";
 import { Fragment } from 'react';
 
-const NewBlock = () => {
-  return (
-    <Fragment>
-      {
-        myData.map(({ name, city, position, id }) => {
-          return <Main key={id} name={name} city={city} position={position} />;
-        })
-      }
-    </Fragment>
-  )
-}
+// const NewBlock = () => {
+//   return (
+//     <Fragment>
+//       {
+//         myData.map(({ name, city, position, id }) => {
+//           return <Main key={id} name={name} city={city} position={position} />;
+//         })
+//       }
+//     </Fragment>
+//   )
+// }
 
 function App() {
-  // const mainBlock = myData.map(({ name, city, position, id }) => {
-  //   return <Main key={id} name={name} city={city} position={position} />;
-  // })
+  const mainBlock = myData.map(({ name, city, position, id }) => {
+    return <Main key={id} name={name} city={city} position={position} />;
+  })
 
   return (
-    <>
+    <Fragment>
       <div>
-        <p>My App</p>
-        <NewBlock />
+        <p>This is My App</p>
       </div>
-    </>
+      <div className="mainBlock_container">
+        {mainBlock}
+      </div>
+    </Fragment>
   );
 }
 
--- react-yt/src/Componets/Main.js
+++ react-yt/src/Componets/Main.js
@@ -1,14 +1,14 @@
-import React from 'react'
-
 function Main({ name, city, position }) {
   return (
     <div>
-      <p>This is the Main Page</p>
-      <ul>
-        <li>{name}</li>
-        <li>{city}</li>
-        <li>{position}</li>
-      </ul>
+      <img src="https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Food image" />
+      <div>
+        <h4>{name}</h4>
+        <p>
+          <span>{city}</span>
+          <span>{position}</span>
+        </p>
+      </div>
     </div>
   )
 }
